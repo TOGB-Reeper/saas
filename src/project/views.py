3e04b997fa1a446b625954121926e74eaa255750
togb-reeper <togbbots@gmail.com>
2024-06-13 00:31:46 +0200

    new

---
+++ src/project/views.py
@@ -0,0 +1,30 @@
+import pathlib
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from visits.models import PageVisit
+
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_view(request, *args, **kwargs):
+    return about_view(request, *args, **kwargs)
+
+def about_view(request, *args, **kwargs):
+    # queryset = qs
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
+    my_title = "About Page"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count": page_qs.count(),
+        "percent": percent,
+        "total_visit_count": qs.count()
+    }
+    html_template = "home.html"
+    PageVisit.objects.create(path=request.path)
+    return render(request, html_template, my_context)
